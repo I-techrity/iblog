7cba73721eea64e83df1bb1e7a8403bde7858330
Hijen EL Khalifi <hijenhek@gmail.com>
2021-08-15 20:48:08 +0100

    tweak voyager link & assets

---
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="app-footer">
+    <div class="site-footer-right">
+        
+            Made by <a href="https://www.iwatch.tn/" target="_blank">I WATCH ORG</a>
+        
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 });
 
 
-Route::group(['prefix' => 'admin'], function () {
+Route::group(['prefix' => 'a'], function () {
     Voyager::routes();
 });
 
